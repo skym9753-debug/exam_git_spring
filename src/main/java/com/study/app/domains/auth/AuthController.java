f38c3839f093a46060f09377c8159f95bccd7bb3
표예찬 <yechan0807@naver.com>
2026-05-08 17:09:39 +0900

    토큰 보내기

--- src/main/java/com/study/app/domains/auth/AuthController.java
+++ src/main/java/com/study/app/domains/auth/AuthController.java
@@ -1,5 +1,8 @@
 package com.study.app.domains.auth;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,7 +12,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.study.app.domains.members.MembersDTO;
-import com.study.app.domains.members.MembersService;
 import com.study.app.utils.JWTUtil;
 
 @RestController
@@ -23,15 +25,19 @@ public class AuthController {
     private JWTUtil jwtUtil;
 
     @PostMapping
-    public ResponseEntity<String> signin(@RequestBody MembersDTO dto) {
+    public ResponseEntity<Map<String,Object>> signin(@RequestBody MembersDTO dto) {
     System.out.println("도착");    
     System.out.println(dto.getId()+":"+dto.getPw());
     	boolean isValid = authService.login(dto);
         if (isValid) {
+        		
             String token = jwtUtil.createToken(dto.getId());
-            return ResponseEntity.ok(token);
+            Map<String,Object> result = new HashMap<>();
+            result.put("token", token);
+			result.put("id", dto.getId());
+            return ResponseEntity.ok(result);
         } else {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("로그인 실패");
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
         }
     }
 }
